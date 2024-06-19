4dd9a5ce92e59b6b756ee3a5575fe228ae60b8b2
SoftwareX Plus <119097812+xcfio@users.noreply.github.com>
2024-06-19 10:21:21 +0600

    set api

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import express from "express"
 import cors from "cors"
-import router from "./router"
 
-const app = express()
+const app = express().use(cors({ optionsSuccessStatus: 200 }))
 
-app.use(cors())
-app.use(router())
+app.get("/api/:date?", (req, res) => {
+    const input = req.params.date
+    if (!input) {
+        const date = new Date()
+        return res.json({ utc: date.toUTCString(), unix: date.getTime() })
+    }
 
-app.listen(7200, () => console.log("Server is running"))
+    const date = new Date(isNaN(Number(input)) ? input : Number(input))
+
+    if (date.toString() === "Invalid Date") {
+        res.json({ error: "Invalid Date" })
+    } else {
+        res.json({ utc: date.toUTCString(), unix: date.getTime() })
+    }
+})
+
+app.listen(3000, () => console.log("Server is running"))
--- src/router/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express"
-
-const router = Router()
-
-export default () => {
-    router.get("/", (req, res) => res.send("Hello World"))
-    return router
-}
